0d696f3ce4d8c4156bedbc326e153abd9a45479c
Grecu Andrei-George <50420491+grecuandrei@users.noreply.github.com>
2019-11-25 03:14:28 +0200

    Added Constants class

---
+++ League_of_OOP/src/com/constants/Constants.java
@@ -0,0 +1,30 @@
+package constants;
+
+public final class Constants {
+    private Constants() { }
+    // HERO
+    public static final int BASE_XP_LEVEL_UP = 250;
+    public static final int MULTIPLIER_LEVEL_UP = 50;
+    public static final int MULTIPLIER_XP_WINNER = 40;
+    public static final int BASE_XP_XP_WINNER = 200;
+    // ROGUE
+    public static final int INITIAL_HP_ROGUE = 600;
+    public static final int LEVEL_HP_MULTIPLIER = 40;
+    public static final float BASE_DMG_BACKSTAB_ROGUE = 200;
+    public static final int BASE_MULTIPLIER_BACKSTAB = 20;
+    public static final float HITS_BACKSTAB = 3;
+    public static final float CRIT_DMG = 1.5f;
+    public static final float LAND_MODIFIER_ROGUE = 1.15f;
+    public static final float BASE_DMG_PARALYSIS_ROGUE = 40;
+    public static final int BASE_MULTIPLIER_PARALYSIS = 10;
+    public static final float BACKSTAB_MULTIPLIER_ROGUE = 1.2f;
+    public static final float PARALYSIS_MULTIPLIER_ROGUE = 0.9f;
+    public static final float BACKSTAB_MULTIPLIER_KNIGHT = 0.9f;
+    public static final float PARALYSIS_MULTIPLIER_KNIGHT = 0.8f;
+    public static final float BACKSTAB_MULTIPLIER_PYRO = 1.25f;
+    public static final float PARALYSIS_MULTIPLIER_PYRO = 1.2f;
+    public static final float BACKSTAB_MULTIPLIER_WIZARD = 1.25f;
+    public static final float PARALYSIS_MULTIPLIER_WIZARD = 1.25f;
+    public static final float PARALISED_BASE_DMG = 40;
+    public static final float PARALISED_MULTIPLIER = 10;
+}
