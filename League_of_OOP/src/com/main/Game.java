6de97d31d325eec0e77f52e3e591a7cdf2fb6434
Grecu Andrei-George <50420491+grecuandrei@users.noreply.github.com>
2019-11-27 21:20:04 +0200

    Modified Game file + Added comments
    Fixed bug where Pyro was dealing never-ending damage.

--- League_of_OOP/src/com/main/Game.java
+++ League_of_OOP/src/com/main/Game.java
@@ -15,11 +15,11 @@ public class Game {
     private ArrayList<Hero> heroes = new ArrayList<>();
     private HeroFactory hf = new HeroFactory();
     public Game() { }
-    
-    public void playGame(final File inputFile, final File outputFile) {
+    public final void playGame(final File inputFile, final File outputFile) {
         try {
             Scanner sr = new Scanner(inputFile);
             // Input
+            // Map instantiation
             int n = sr.nextInt();
             int m = sr.nextInt();
             Map map = Map.getInstance(n, m);
@@ -30,7 +30,7 @@ public class Game {
                     map.addTerrain(s.charAt(j));
                 }
             }
-            // make heroes
+            // Build heroes
             int nrPlayers = sr.nextInt();
             sr.nextLine();
             for (int i = 0; i < nrPlayers; ++i) {
@@ -38,92 +38,76 @@ public class Game {
                 int a = sr.nextInt();
                 int b = sr.nextInt();
                 sr.nextLine();
+                // add the hero to the pool
                 heroes.add(hf.getHero(c));
+                // set the coordinates
                 heroes.get(heroes.size() - 1).setX(a);
                 heroes.get(heroes.size() - 1).setY(b);
-                heroes.get(heroes.size() - 1).setTerrainUnderFeet(map.getTerrain(heroes.get(heroes.size() - 1).getX(), heroes.get(heroes.size() - 1).getY()));
+                // and get the terrain that he is on
+                heroes.get(heroes.size() - 1).setTerrainUnderFeet(
+                        map.getTerrain(heroes.get(heroes.size() - 1).getX(),
+                                heroes.get(heroes.size() - 1).getY()));
             }
-            
-            // rounds
+            // Do the rounds - play the game
             int nrRounds = sr.nextInt();
             sr.nextLine();
             for (int i = 0; i < nrRounds; ++i) {
-                System.out.println("-------------Runda " + i + "----------------");
-                for (Hero h:heroes) {
-                    System.out.println(h.getName() + " " + h.getHp() + " " + h.getX() + " " + h.getY());
-                }
-                
                 String moves = sr.nextLine();
-                
-                System.out.println(moves);
-                
-                for (int j = 0; j < moves.length(); ++j) {
-                    System.out.println(j + heroes.get(j).getName() + " " + heroes.get(j).isIncap() + " " + heroes.get(j).isParalised() + " " + heroes.get(j).getX() + " " + heroes.get(j).getY());
-                    
-                    if (!heroes.get(j).isIncap() && !heroes.get(j).isParalised()) {
+                int j = 0;
+                for (Hero h:heroes) {
+                    // if it is not dead, incapacitated or paralized, move the player
+                    if (!h.isDead() && !h.isIncap() && !h.isParalyzed()) {
+                        if (h.isIgnited()) {
+                            h.setRoundsApply(h.getRoundsApply() - 1);
+                        }
                         moveHero(j, moves.charAt(j), map);
-                    } else if (heroes.get(j).isIncap()) {
-                        heroes.get(j).setIncap(false);
+                    } else if (!h.isDead()) {
+                        // if it was incapacitated, "liberate" him
+                        if (h.isIncap()) {
+                            heroes.get(j).setIncap(false);
+                        }
+                        h.setRoundsApply(h.getRoundsApply() - 1);
                     }
-                    
-                    System.out.println(heroes.get(j).getName() + " " + heroes.get(j).isIncap() + " " + heroes.get(j).isParalised());
+                    j++;
                 }
-                
-                System.out.println();
-                System.out.println("Hp inainte de overtime");
+                // apply for every player the overtime damage
                 for (Hero h:heroes) {
-                    System.out.println(h.getName() + " " + h.getHp() + " " + h.getX() + " " + h.getY());
+                    if (!h.isDead() && h.getRoundsApply() >= 0) {
+                        applyOvertimeDmg(h);
+                    }
                 }
-                
+                // for every player
                 for (Hero h:heroes) {
-                    applyOvertimeDmg(h);
-                }
-                
-                System.out.println("Hp dupa de overtime");
-                for (Hero h:heroes) {
-                    System.out.println(h.getName() + " " + h.getHp());
-                }
-                
-                for (Hero h:heroes) {
+                    // if it is not dead or had a fight this round
                     if (!h.isAttacked() && !h.isDead()) {
+                        // check if another hero is on the same terrain and play the fight
                         for (Hero h1 : heroes) {
                             if (!h.equals(h1) && h.getX() == h1.getX()
                                     && h.getY() == h1.getY() && !h1.isDead()) {
+                                // set that there are fighting
                                 h.setAttacked(true);
                                 h1.setAttacked(true);
-
-                                System.out.println("Hp inainte fight " + h.getHp() + " " + h1.getHp());
-                                
-                                h.setDmgInflicted(Math.round(h1.calculateFlatDmg())); // W. (R)
-                                h1.setDmgInflicted(Math.round(h.calculateFlatDmg())); // R. (W)
-                                
-                                float dmgPlayer1 = Math.round(h.acceptAttack(h1)); // W.accept(R) -> R.attack(W)
-                                float dmgPlayer2 = Math.round(h1.acceptAttack(h)); // R.accept(W) -> W.attack(R)
-
-                                System.out.println(dmgPlayer1 + " " + dmgPlayer2);
-                                
+                                // calculate the damage if one of them is a wizard
+                                h.setDmgInflicted(Math.round(h1.calculateFlatDmg()));
+                                h1.setDmgInflicted(Math.round(h.calculateFlatDmg()));
+                                // calculate the damage that will be dispersed on the other
+                                // h.accept(h1) -> h1.attack(h)
+                                float dmgPlayer1 = Math.round(h.acceptAttack(h1));
+                                // h1.accept(h) -> h.attack(h1)
+                                float dmgPlayer2 = Math.round(h1.acceptAttack(h));
+                                // deal the damage
                                 h.dealDMG(h1, dmgPlayer2);
                                 h1.dealDMG(h, dmgPlayer1);
-                                
+                                // give xp and if needed update the health
                                 h.levelUP();
                                 h1.levelUP();
-                                
-                                if (h.isDead()) {
-                                    System.out.println("Winner is " + h1.getName() + h1.getXp() + h1.getLevel());
-                                } else if (h1.isDead()){
-                                    System.out.println("Winner is " + h.getName() + h.getXp() + h.getLevel());
-                                }
-                                System.out.println("Hp dupa fight " + h.getHp() + " " + h.isIncap() + " " + h1.getHp() + " " + h1.isIncap());
-                                System.out.println();
                             }
                         }
                     }
                 }
+                // uncheck the field that says they were fighting for this rounds\
                 for (Hero h:heroes) {
                     h.setAttacked(false);
-                    
-                    System.out.println(h.getName() + " " + h.getHp()
-                            + " " + h.getX() + " " + h.getY());
                 }
             }
             sr.close();
@@ -132,11 +116,15 @@ public class Game {
             e.printStackTrace();
         }
     }
+    // function that moves the player, based on the input
     private void moveHero(final int i, final char c, final Map map) {
         switch (c) {
             case 'U':
+                // update the x
                 heroes.get(i).setX(heroes.get(i).getX() - 1);
+                // update the y
                 heroes.get(i).setY(heroes.get(i).getY());
+                // update the terrain that he is standing on
                 heroes.get(i).setTerrainUnderFeet(map.getTerrain(heroes.get(i).getX(),
                         heroes.get(i).getY()));
                 break;
@@ -162,17 +150,19 @@ public class Game {
                 break;
         }
     }
+    // function that applies the overtime damage for every player
     private void applyOvertimeDmg(final Hero h) {
-        System.out.println("Damage overtime " + h.getDmgOvertime());
-        
-        if (h.getRoundsApply() > 0) {
-            h.setHp(h.getHp() - h.getDmgOvertime());
-            h.setRoundsApply(h.getRoundsApply() - 1);
+        h.setHp(h.getHp() - (int) h.getDmgOvertime());
+        // if the player is paralyzed and the rounds of damage are over
+        if (h.isParalyzed() && h.getRoundsApply() == 0) {
+            h.setParalyzed(false, 0, 0);
         }
+        // if the damage was too much, kill the player
         if (h.getHp() <= 0) {
             h.setDead(true);
         }
     }
+    // function that prints in the file the output
     private void writeToFile(final File outputFile)
     throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
@@ -182,7 +172,7 @@ public class Game {
                 writer.append(" dead");
             } else {
                 writer.write(" " + h.getLevel() + " " + h.getXp()
-                        + " " + Math.round(h.getHp()) + " " + h.getX() + " " + h.getY());
+                        + " " + h.getHp() + " " + h.getX() + " " + h.getY());
             }
             writer.newLine();
         }
