5eb0c2a06055dc6e95481689224cf743ada2affa
Grecu Andrei-George <50420491+grecuandrei@users.noreply.github.com>
2019-12-29 22:41:41 +0200

    Updated the constants

--- League_of_OOP/src/com/constants/Constants.java
+++ League_of_OOP/src/com/constants/Constants.java
@@ -2,11 +2,14 @@ package constants;
 
 public final class Constants {
     private Constants() { }
+    // GAME
+    public static final int ANGEL_PARTS = 3;
     // HERO
     public static final int BASE_XP_LEVEL_UP = 250;
     public static final int MULTIPLIER_LEVEL_UP = 50;
     public static final int MULTIPLIER_XP_WINNER = 40;
     public static final int BASE_XP_XP_WINNER = 200;
+    public static final float PRECISION = 1000f;
     // ROGUE
     public static final int INITIAL_HP_ROGUE = 600;
     public static final int LEVEL_HP_MULTIPLIER_ROGUE = 40;
@@ -27,6 +30,8 @@ public final class Constants {
     public static final float PARALYSIS_MULTIPLIER_WIZARD = 1.25f;
     public static final int MAX_ROUNDS = 6;
     public static final int MIN_ROUNDS = 3;
+    public static final int R_STRAT_LOW = 7;
+    public static final int R_STRAT_HIGH = 5;
     // WIZARD
     public static final int INITIAL_HP_WIZARD = 400;
     public static final int LEVEL_HP_MULTIPLIER_WIZARD = 30;
@@ -44,6 +49,8 @@ public final class Constants {
     public static final float DEFLECT_MULTIPLIER_PYRO = 1.3f;
     public static final float DRAIN_MULTIPLIER_WIZARD = 1.05f;
     public static final float PERCENTAGE_BASE_HP = 0.3f;
+    public static final int W_STRAT_LOW = 4;
+    public static final int W_STRAT_HIGH = 2;
     // KNIGHT
     public static final int INITIAL_HP_KNIGHT = 900;
     public static final int LEVEL_HP_MULTIPLIER_KNIGHT = 80;
@@ -62,6 +69,8 @@ public final class Constants {
     public static final float SLAM_MULTIPLIER_PYRO = 0.9f;
     public static final float EXECUTE_MULTIPLIER_WIZARD = 0.8f;
     public static final float SLAM_MULTIPLIER_WIZARD = 1.05f;
+    public static final int K_STRAT_LOW = 3;
+    public static final int K_STRAT_HIGH = 2;
     // PYROMANCER
     public static final int INITIAL_HP_PYRO = 500;
     public static final int LEVEL_HP_MULTIPLIER_PYRO = 50;
@@ -81,4 +90,72 @@ public final class Constants {
     public static final int IGNITE_OVT_DMG = 50;
     public static final int IGNITE_OVT_MULTIPLIER = 30;
     public static final int IGNITE_ROUNDS = 2;
+    public static final int P_STRAT_LOW = 4;
+    public static final int P_STRAT_HIGH = 3;
+    // DAMAGEANGEL
+    public static final float DMGA_KNIGHT_MOD = 0.15f;
+    public static final float DMGA_PYRO_MOD = 0.2f;
+    public static final float DMGA_ROGUE_MOD = 0.3f;
+    public static final float DMGA_WIZARD_MOD = 0.4f;
+    // DARKANGEL
+    public static final int DARKA_KNIGHT = 40;
+    public static final int DARKA_PYRO = 30;
+    public static final int DARKA_ROGUE = 10;
+    public static final int DARKA_WIZARD = 20;
+    // DRACULA
+    public static final float DRAC_KNIGHT_MOD = 0.2f;
+    public static final float DRAC_PYRO_MOD = 0.3f;
+    public static final float DRAC_ROGUE_MOD = 0.1f;
+    public static final float DRAC_WIZARD_MOD = 0.4f;
+    public static final int DRAC_KNIGHT = 60;
+    public static final int DRAC_PYRO = 40;
+    public static final int DRAC_ROGUE = 35;
+    public static final int DRAC_WIZARD = 20;
+    // GOODBOY
+    public static final float GB_KNIGHT_MOD = 0.4f;
+    public static final float GB_PYRO_MOD = 0.5f;
+    public static final float GB_ROGUE_MOD = 0.4f;
+    public static final float GB_WIZARD_MOD = 0.3f;
+    public static final int GB_KNIGHT = 20;
+    public static final int GB_PYRO = 30;
+    public static final int GB_ROGUE = 40;
+    public static final int GB_WIZARD = 50;
+    // LEVELUPANGEL
+    public static final float LVL_KNIGHT_MOD = 0.1f;
+    public static final float LVL_PYRO_MOD = 0.2f;
+    public static final float LVL_ROGUE_MOD = 0.15f;
+    public static final float LVL_WIZARD_MOD = 0.25f;
+    // LIFEGIVER
+    public static final int LG_KNIGHT = 100;
+    public static final int LG_PYRO = 80;
+    public static final int LG_ROGUE = 90;
+    public static final int LG_WIZARD = 120;
+    // SMALANGEL
+    public static final float SA_KNIGHT_MOD = 0.1f;
+    public static final float SA_PYRO_MOD = 0.15f;
+    public static final float SA_ROGUE_MOD = 0.05f;
+    public static final float SA_WIZARD_MOD = 0.1f;
+    public static final int SA_KNIGHT = 10;
+    public static final int SA_PYRO = 15;
+    public static final int SA_ROGUE = 20;
+    public static final int SA_WIZARD = 25;
+    // SPAWNER
+    public static final int SPAWNER_KNIGHT = 200;
+    public static final int SPAWNER_PYRO = 150;
+    public static final int SPAWNER_ROGUE = 180;
+    public static final int SPAWNER_WIZARD = 120;
+    // XPANGEL
+    public static final int XP_KNIGHT = 45;
+    public static final int XP_PYRO = 50;
+    public static final int XP_ROGUE = 40;
+    public static final int XP_WIZARD = 60;
+    // STRATEGIES CONSTANTS
+    public static final float MOD_04 = 0.4f;
+    public static final float MOD_05 = 0.5f;
+    public static final float MOD_01 = 0.1f;
+    public static final float MOD_025 = 0.25f;
+    public static final float MOD_02 = 0.2f;
+    public static final float MOD_07 = 0.7f;
+    public static final float MOD_03 = 0.3f;
+    public static final float MOD_06 = 0.6f;
 }
